97420c315759b28c0743e13e62c335e8be583b67
A7yanG <yang_shi1014@163.com>
2022-05-21 16:53:12 +0800

    增加了矩形拟合算法

--- test.cpp
+++ test.cpp
@@ -1,69 +1,178 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <ctime>
-#include <numeric>
-#include <random>
-#include <cmath>
-#include <queue>
+#include <stdlib.h>
+#include <vector>
 using namespace std;
-int search1(string start, string end)
+
+//结构体
+typedef unsigned char byte;
+typedef struct Point
 {
-    int count = 0;
-    for (int i = 0; i < start.length(); i++)
-    {
-        if (start[i] != end[i])
-            count++;
-    }
-    if (count == 1)
-        return 1;
-    return 0;
-}
-int minMutation(string start, string end, vector<string> &bank)
+    byte x;
+    byte y;
+};
+typedef struct Individual
 {
-    queue<string> q;
-    q.push(start);
-    int count = 0;
-    while (!q.empty())
+    vector<Point> Points;
+    double fitness;
+};
+
+//全局变量
+vector<Individual> PopMain;
+static int MainSize = 100;        //种群大小
+static int IndividualSize = 2000; //个体大小，即撒点数量
+static double CrossRate = 0.9;
+static double VariationRate = 0.01;
+//函数定义
+void GeneratePopulation();
+void GenerateIndividual(Individual &individual);
+void CalcIndividualFitness(Individual &individual);
+void CalcFitness();
+void Select();
+void CrossOver();
+void Variation();
+bool IfPointValid(Point point);
+
+
+//函数实现
+void PrintPopMain()
+{
+    for(int i=0;i<MainSize;i++)
     {
-        int size = q.size();
-        for (vector<string>::iterator it = bank.begin(); it < bank.end(); it++)
-            cout<<*it<<" ";
-        cout<<endl;
-        while (size--)
+        cout<<"Individual"<<i<<":"<<endl;
+        for(int j=0;j<IndividualSize;j++)
         {
-            string cur = q.front();
-            q.pop();
-            if (cur == end)
-                return count;
-            vector<string>::iterator it = bank.begin();
-            for (; it !=bank.end(); it++)
-            {
-                cout<<*it<<endl;
-                if (search1(cur, *it))
-                {
-                    q.push(*it);
-                    bank.erase(it);
-                    it--;
-                }
-            }
-            cout<<"//"<<endl;
+            cout<<(int)PopMain[i].Points[j].x<<" "<<(int)PopMain[i].Points[j].y<<",";
         }
-        count++;
+        cout<<endl;
     }
-    return -1;
+}
+void GeneratePopulation()
+{
+
+    srand(int(time(0)));
+    PopMain.clear();
+    for (int i = 0; i < MainSize; i++)
+    {
+
+        Individual individual;
+        PopMain.push_back(individual);
+        GenerateIndividual(PopMain[i]);
+    }
+}
+void GenerateIndividual(Individual &individual)
+{
+    for (int i = 0; i < IndividualSize; i++)
+    {
+        Point point;
+        point.x = rand() % 256;
+        point.y = rand() % 256;
+        individual.Points.push_back(point);
+    }
+}
+void CalcIndividualFitness(Individual &individual)
+{
+    int validPoint = 0;
+    for (int i = 0; i < IndividualSize; i++)
+    {
+        if (IfPointValid(individual.Points[i]))
+        {
+            validPoint++;
+        }
+    }
+    individual.fitness = validPoint;
+}
+void CalcFitness()
+{
+    for (int i = 0; i < MainSize; i++)
+    {
+        CalcIndividualFitness(PopMain[i]);
+    }
+}
+void Select()
+{
+    vector<Individual> PopTemp=PopMain;
+    PopMain.clear();
+    int SumFitness = 0;
+    for (int i = 0; i < MainSize; i++)
+        SumFitness += PopTemp[i].fitness;
+    for (int t = 0; t < MainSize; t++) //轮盘赌
+    {
+        int temp = rand() % SumFitness; //随机生成数
+        int SumTemp = 0;
+        for (int i = 0; i < MainSize; i++)
+        {
+            SumTemp += PopTemp[i].fitness;
+            if (SumTemp >= temp) //累加值>=随机生成的值
+            {
+                PopMain.push_back(PopTemp[i]);
+                break;
+            }
+        }
+    }
+}
+void CrossOver()
+{
+    for (int i = 0; i < MainSize; i+=2)
+    {
+        double r = rand() % 100 * 1.0 / 100;
+        if (r < CrossRate)
+        {
+            int start = rand() % IndividualSize;
+            int stop = rand() % IndividualSize;
+            while (start == stop)
+                stop = rand() % IndividualSize;
+            if (stop < start)
+            {
+                int temp = stop;
+                stop = start;
+                start = temp;
+            }
+            for (int j = start; j <= stop; j++)
+            {
+                Point p1 = PopMain[i].Points[j];
+                PopMain[i].Points[j] = PopMain[i + 1].Points[j];
+                PopMain[i + 1].Points[j] = p1;
+            }
+        }
+    }
+}
+void Variation()
+{
+    for (int i = 0; i < MainSize; i++)
+    {
+        double r1 = rand() % 100 * 1.0 / 100;
+        if (r1 < VariationRate)
+        {
+            //cout<<i<<"变异了"<<endl;
+            int r = rand() % (16 * IndividualSize);
+            int index = r / 16; //变异点的下标
+            int bitindex = r % 16 % 8;//变异点的位
+            //cout<<"index:"<<index<<" bit:"<<bitindex<<endl;
+            //cout<<(int)PopMain[i].Points[index].x<<' '<<(int)PopMain[i].Points[index].y<<endl;
+            if (r % 16 / 8) //变异y
+            {
+                PopMain[i].Points[index].y^= (1 << bitindex);
+            }
+            else
+            {
+                PopMain[i].Points[index].x^= (1 << bitindex);
+            }
+            //cout<<(int)PopMain[i].Points[index].x<<' '<<(int)PopMain[i].Points[index].y<<endl;
+        }
+    }
+}
+bool IfPointValid(Point point)
+{
+    return true;
 }
 
 int main()
 {
-    vector<string> bank;
-    //cout<<search1("AACCGGTT","AACCGGTT")<<endl;
-    string start="AACCGGTT";
-    string end="AAACGGTA";
-    bank.push_back("AACCGGTA");
-    bank.push_back("AACCGCTA");
-    bank.push_back(end);
-    cout<<minMutation(start,end,bank)<<endl;
-    system("pause");
+    GeneratePopulation();
+    CalcFitness();
+    Select();
+    CrossOver();
+    Variation();
     return 0;
 }
